29d14414f8ecd87502b4d5dff3f85586d9c135cc
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-24 23:37:23 +0530

    docker

--- frontend/src/app/services/product.ts
+++ frontend/src/app/services/product.ts
@@ -10,7 +10,7 @@ export class ProductService {
 
   private http = inject(HttpClient);
 
-  private apiUrl = 'http://localhost:8080/products';
+  private apiUrl = 'http://174.129.112.40:8080/products';
 
   getAllProducts(): Observable<Product[]> {
 
