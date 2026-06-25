9cf3e60b5d3c089da44e671d15e83e517ed22f59
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-25 19:36:37 +0530

    docker

--- frontend/src/app/services/order.ts
+++ frontend/src/app/services/order.ts
@@ -3,7 +3,7 @@ import {
   HttpClient,
   HttpHeaders
 } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn:'root'
