02e2135f2d448492fe2844e892751b8e07f7bac1
srinu-info <sriuvasuluyaram@gmail.com>
2026-06-26 12:34:50 +0530

    docker

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://100.31.238.92:8080'
+  apiUrl: 'http://54.221.191.219:8080'
 };
 
 // for i in api-gateway auth-service product-service cart-service order-service feedback-service frontend
